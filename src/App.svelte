8282d7fb0b457afa48f995a3c35421ac53314d5f
Jonathan Stegall <jstegall@minnpost.com>
2022-03-29 14:20:58 -0500

    keep the s3 url

--- src/App.svelte
+++ src/App.svelte
@@ -90,7 +90,7 @@
 		for local data (if flask app is running):
 		http://0.0.0.0:5001/candidate-tracker/json/
 		*/
-		let res = await fetch(`http://0.0.0.0:5001/candidate-tracker/json/`);
+		let res = await fetch(`https://s3.amazonaws.com/data.minnpost/projects/spreadsheets/1dQxzfWG1a97zRzKN3QJQnGG4H3M4KhOGoi30_E62-2o-Categories|House|Senate-custom.json`);
 		let data = await res.json();
 		items = data
 		return items;
