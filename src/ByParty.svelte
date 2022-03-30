0796252ac2e61faeb6cd7f45168b011c9348929e
Jonathan Stegall <jstegall@minnpost.com>
2022-03-30 16:49:16 -0500

    start fixing the party filter

--- src/ByParty.svelte
+++ src/ByParty.svelte
@@ -6,7 +6,7 @@
 	export let params;
 
 	// the races from App.svelte
-	let races = items.races;
+	let districts = items.districts;
 
 	// what party/parties do we want
     let parties = [];
@@ -19,21 +19,40 @@
     }
 
 	// create a list of candidates for a party
-	let party_candidates = function(party, office = '') {
-		if (office != '') {
+	let party_district_candidates = function(chamber, party = '', district = '') {
+
+		/*let all_parties = [...new Set(items.candidates.filter(function(item, index) {
+			if ( item.party ) {
+				return item.party;
+			}
+		}).map(function(obj) { return obj.party; }))];*/
+
+		if (district != '') {
 			return items.candidates.filter(
-				item => item["party"].indexOf(party) !== -1 && item["office-sought"].indexOf(office) !== -1
+				function(item, index) {
+					if (item.party) {
+						if ( item["chamber"] === chamber && item["party"].indexOf(party) !== -1 && item["district"] == district ) {
+							return item;
+						}
+					}
+				}
 			)
 		} else {
 			return items.candidates.filter(
-				item => item["party"].indexOf(party) !== -1
+				function(item, index) {
+					if (item.party) {
+						if ( item["chamber"] === chamber &&item["party"].indexOf(party) !== -1 && item["district"] == district ) {
+							return item;
+						}
+					}
+				}
 			)
 		}
 	}
 
-	// the distinct races from this list of candidates
-	let party_candidate_races = function(candidates) {
-		return [...new Set(candidates.map(value => value["office-sought"]))];
+	// the distinct districts from this list of candidates
+	let party_candidate_districts = function(candidates) {
+		return [...new Set(candidates.map(value => value["district"]))];
 	}
 
 	// single candidate template
@@ -44,33 +63,33 @@
 
 </script>
 
-{#if parties.length == 1}
-	<aside class="m-search-result-info">
-		{#if items["searchTerm"] != ""}
-			Showing {party_candidates(parties[0]).length} {#if party_candidates(parties[0]).length == 1}search result{:else}search results{/if} for <strong>{items.searchTerm}</strong> within all {parties[0]} candidates.
-		{:else}
-			Showing {party_candidates(parties[0]).length} <strong>{parties[0]}</strong> {#if party_candidates(parties[0]).length == 1}candidate{:else}candidates{/if} in {party_candidate_races(party_candidates(parties[0])).length} {#if party_candidate_races(party_candidates(parties[0])).length == 1}race{:else}races{/if}.
-		{/if}
-	</aside>
-{/if}
 
 {#each parties as party, key}
-	<section class="district-listing">
+
+	<section class="party-listing">
 		{#if parties.length > 1}
 			<h2 class="m-archive-header party-{items.all_party_ids[key]}">{party}</h2>
 		{/if}
-		{#each races as race, key}
-			{#if party_candidates(party, race.office).length > 0}
-			<h3 class="m-archive-header">{race.office}</h3>
-			{#if race.blurb}
-				<p>{@html race.blurb}</p>
-			{/if}
-				<section class="m-archive m-archive-homepage m-zone m-zone-homepage-more-top candidates-list">
-					{#each party_candidates(party, race.office) as candidate}
-						<Candidate candidate = {candidate} />
-					{/each}
-				</section>
-			{/if}
+
+		{#each items.chambers as chamber}
+			<section class="chamber-listing">
+				<h2 class="m-archive-header m-chamber-header">{chamber}</h2>
+				{#if chamber.blurb}
+					<p>{@html chamber.blurb}</p>
+				{/if}
+		
+				{#each districts as district_region, key}
+					{#if party_district_candidates(chamber, party, district_region.district).length > 0}
+						<article class="m-district">
+							{district_region.chamber} {district_region.district} {district_region.region}
+							<h3 class="m-archive-header">{district_region.district}</h3>
+							{#each party_district_candidates(chamber, party, district_region.district) as candidate}
+								<Candidate candidate = {candidate} />
+							{/each}
+						</article>
+					{/if}	
+				{/each}
+			</section>
 		{/each}
 	</section>
 {/each}
