1d74ffa1fa81fe5e2c1f376f202711d73bc3e12b
Jonathan Stegall <jstegall@minnpost.com>
2022-03-31 14:07:48 -0500

    route the region filter

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import AllCandidates from './AllCandidates.svelte'
 import ByChamber from './ByChamber.svelte'
 //import ByDistrict from './ByDistrict.svelte'
 import ByParty from './ByParty.svelte'
+import ByRegion from './ByRegion.svelte'
 //import Error from './Error.svelte'
 
 export default [
@@ -32,5 +33,13 @@ export default [
   {
     path: '/by-party/:party',
     component: ByParty
+  },
+  {
+    path: '/by-region',
+    component: ByRegion,
+  },
+  {
+    path: '/by-region/:region',
+    component: ByRegion
   }
 ]
